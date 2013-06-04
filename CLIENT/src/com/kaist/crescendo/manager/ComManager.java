93ccc03e6b1c7b7f5098de1018b58f353e65db8a
ehyewony <ehyewony@gmail.com>
2013-06-05 00:53:03 +0900

    fix ComManager - hyewon.eum

--- CLIENT/src/com/kaist/crescendo/manager/ComManager.java
+++ CLIENT/src/com/kaist/crescendo/manager/ComManager.java
@@ -1,13 +1,10 @@
 package com.kaist.crescendo.manager;
 
-import org.json.JSONException;
 import org.json.JSONObject;
 
 public class ComManager {
 	private CommunicationInterface handler;
-	private int msgId;
-	private JSONObject jsonObject;
-	
+
 	public ComManager() {
 		// choose FileEmulator or RealSocket
 		handler = new FileEmulator();
@@ -15,27 +12,7 @@ public class ComManager {
 	
 	public String ProcessMsg(JSONObject msg) {
 		int result = MsgInfo.STATUS_OK;
-		String userId = null;
-		String jsonString = null;
-		
-		try {
-			msgId = msg.getInt(MsgInfo.MSGID_LABEL);
-			jsonObject = msg;
-		} catch (JSONException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		if(msgId == MsgInfo.REGISTER_ID) {
-			JSONObject uData = null;
-			
-			try {
-				uData = (JSONObject) jsonObject.get(MsgInfo.MSGBODY_LABEL);
-				userId = uData.getString(MsgInfo.USERID_LABEL);
-			} catch (Exception e) {
-				// TODO: handle exception
-			}
-		}
+		String jsonString = null;	
 		
 		result = handler.write(msg);
 		
