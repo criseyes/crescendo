562e63a2fce222a28dfe152695659196ddca2e7c
ehyewony <ehyewony@gmail.com>
2013-06-07 17:17:44 +0900

    edit scrollview - hyewon.eum

--- CLIENT/src/com/kaist/crescendo/activity/StatusActivity.java
+++ CLIENT/src/com/kaist/crescendo/activity/StatusActivity.java
@@ -2,6 +2,7 @@ package com.kaist.crescendo.activity;
 
 import com.kaist.crescendo.R;
 import com.kaist.crescendo.data.GraphView;
+import com.kaist.crescendo.utils.MyStaticValue;
 
 import android.os.Bundle;
 
@@ -15,10 +16,10 @@ public class StatusActivity extends UpdateActivity {
 		setContentView(R.layout.activity_status);
 		setTitle(R.string.str_show_status);
 		
-		float[] values = new float[] { 2.0f,1.5f, 2.5f, 1.0f , 3.0f };
-		String[] verlabels = new String[] { "great", "ok", "bad" };
+		float[] values = new float[] { 2.0f,1.5f, 2.5f, 1.0f , 3.0f, 30.f };
+		String[] verlabels = new String[] { "great", "ok", "bad", "nice", "good" };
 		String[] horlabels = new String[] { "today", "tomorrow", "next week", "next month" };
-		GraphView graphView = new GraphView(this, values, "GraphViewDemo",horlabels, verlabels, GraphView.LINE);
+		GraphView graphView = new GraphView(this, values, MyStaticValue.myId+"¥‘¿« ¡¯√¥µµ" ,horlabels, verlabels, GraphView.LINE);
 		setContentView(graphView);
 		
 	}
